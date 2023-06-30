63f6e209fc7fcb0c32e98ff5aebd03b6184ee3cd
Eunsoo Lee <eun.boo@outlook.com>
2023-06-29 22:56:12 -0700

    20230629.0.6

---
+++ _includes/toggle-menu.js
@@ -0,0 +1,12 @@
+
+
+$(function () {
+    $('.toggle-menu li').click(function () {
+        $(this).toggleClass(' active ');
+        $(this).siblings().removeClass(' active ');
+        $('.submenu').stop().slideUp();
+        $('.active .submenu').stop().slideDown();
+        return false;
+    });
+});
+
